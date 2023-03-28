e3c3a78ba368e711f981ab4bf5e2b996b0c7c4f9
Truong vi <vinlt@antsomi.com>
2023-03-28 12:10:45 +0700

    chore(Divider.stories.tsx): update links text in Vertical story of Divider component

--- src/components/Divider/Divider.stories.tsx
+++ src/components/Divider/Divider.stories.tsx
@@ -111,9 +111,9 @@ export const Vertical: ComponentStory<any> = () => (
   <>
     Text Vertical
     <Divider type="vertical" />
-    <a href="#">Link 1</a>
+    <a href="#">Link 1 123</a>
     <Divider type="vertical" />
-    <a href="#">Link 2</a>
+    <a href="#">Link 2 234312</a>
   </>
 );
 
